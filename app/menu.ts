244c4810916c8363ff7140632f9b111f159985b9
Sunny Dave <sunny.dave@sproutlogix.com>
2023-01-09 22:49:14 -0500

    Updated chrome version Updated menu items

--- app/menu.ts
+++ app/menu.ts
@@ -57,14 +57,14 @@ export default class MenuBuilder {
       label: 'Electron',
       submenu: [
         {
-          label: 'About ElectronReact',
+          label: 'About',
           selector: 'orderFrontStandardAboutPanel:',
         },
         { type: 'separator' },
         { label: 'Services', submenu: [] },
         { type: 'separator' },
         {
-          label: 'Hide ElectronReact',
+          label: 'Hide',
           accelerator: 'Command+H',
           selector: 'hide:',
         },
