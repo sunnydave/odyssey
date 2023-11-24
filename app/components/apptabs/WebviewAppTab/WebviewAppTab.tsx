b37eaf7f75471b763b2bb5bfcad37c926d0fb18a
Sunny Dave <sunny.dave@sproutlogix.com>
2023-11-24 11:05:08 -0500

    Updated chrome version in the app

--- app/components/apptabs/WebviewAppTab/WebviewAppTab.tsx
+++ app/components/apptabs/WebviewAppTab/WebviewAppTab.tsx
@@ -131,7 +131,7 @@ export default function WebviewAppTab(props: any) {
       webpreferences="allowRunningInsecureContent, nativeWindowOpen=yes"
       preload="./notification-preload.js"
       partition={appSeperateSession ? `persist:${appId}` : `persist:odyssey`}
-      useragent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+      useragent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
     />
   );
 }
